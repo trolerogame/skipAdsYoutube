324a4c78346f17319d75875965e12e48b8c4860c
trolerogame <danielgcampos.04@gmail.com>
2024-06-17 16:38:39 -0300

    add console active and remove transition

--- content.js
+++ content.js
@@ -3,10 +3,12 @@ let interval = null
 chrome.storage.onChanged.addListener(function(changes, areaName) {
     if (areaName === 'local' && 'skipAdEnabled' in changes) {
         if(changes.skipAdEnabled.newValue){
+            console.log('active')
             interval = setInterval( () => {
                 document.querySelector(".ytp-skip-ad-button") && document.querySelector(".ytp-skip-ad-button").click()
             } , 100)
         }else{
+            console.log('inactive')
             clearInterval(interval)
             interval = null
         }
